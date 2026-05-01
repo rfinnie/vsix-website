7f73567555ede452ff20f04c82b565ed2b83673d
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:34:50 -0700

    Add explicit build-system defs, stub setup.py for setuptools <61.0.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: vsix-website
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/finnix/vsix-website
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
